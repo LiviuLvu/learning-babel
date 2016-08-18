46fb5e2893505946e3dc2302b82d52d86367e072
Liviu Iancu <liviuiancu@gmx.com>
2016-08-18 23:48:19 +0300

    Completed promises video lesson

---
+++ build/promise.js
@@ -0,0 +1,40 @@
+// promises in es6
+'use strict';
+
+// let myPromise = new Promise((resolve, reject) => {
+//    setTimeout(() => {
+//       resolve('Good to go!');
+//    }, 500);
+
+// setTimeout(() => {
+//    reject('There is an elephant in the room');
+// }, 500);
+// });
+
+// myPromise.then((res) => {
+//       console.log(res);
+//    })
+//    .catch((err) => {
+//       console.log(err);
+//    })
+// });
+
+// let myPromise2 = new Promise((resolve, reject) => {
+//    setTimeout(() => {
+//       resolve('This looks even more promising');
+//    }, 1000);
+// });
+
+// Promise.all([myPromise, myPromise2])
+//    .then((data) => {
+//       console.log(data);
+//    });
+// });
+
+fetch('http://api.icndb.com/jokes/random/10').then(function (res) {
+   res.json().then(function (data) {
+      console.log(data);
+   });
+}).catch(function (err) {
+   console.log(err);
+});
